e05d898993ef76934c6d85f05fa50b15745dbba7
AceArchar <error404.mainmethod@gmail.com>
2022-10-28 21:55:08 +0600

    Init

---
+++ app/Models/Shedule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shedule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'location_id',
+        'time_from',
+        'time_to',
+        'status',
+    ];
+
+    /**
+     * Get the user associated with the Shedule
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    /**
+     * Get the location associated with the Shedule
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function location()
+    {
+        return $this->hasOne(Location::class);
+    }
+}
